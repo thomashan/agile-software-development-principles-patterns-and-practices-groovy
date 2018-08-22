ebbf8bacb72e880ae471e6cce06af44cd811f476
Thomas Han <thomas.han@live.com>
2018-08-22 15:10:25 +1000

    calculate correct grossPay with pro rata salary and sales receipt date filter

--- payroll-case-study/src/main/groovy/thomashan/github/io/payroll/classification/CommissionedClassification.groovy
+++ payroll-case-study/src/main/groovy/thomashan/github/io/payroll/classification/CommissionedClassification.groovy
@@ -3,6 +3,11 @@ package thomashan.github.io.payroll.classification
 import thomashan.github.io.payroll.PayCheque
 import thomashan.github.io.payroll.SalesReceipt
 
+import java.time.Period
+
+import static java.time.temporal.ChronoUnit.DAYS
+import static thomashan.github.io.payroll.DateTimeUtil.daysInMonth
+
 class CommissionedClassification implements PaymentClassification {
     private final List<SalesReceipt> salesReceipts = []
     final double salary
@@ -19,6 +24,13 @@ class CommissionedClassification implements PaymentClassification {
 
     @Override
     double calculatePay(PayCheque payCheque) {
-        return salesReceipts.collect { it.amount * commissionRate / 100 }.sum() + salary
+        long daysInPayCycle = Period.between(payCheque.payPeriodStartDate, payCheque.payPeriodEndDate).get(DAYS)
+        double proRataSalary = daysInPayCycle / daysInMonth(payCheque.payDate) * salary
+        double commissions = salesReceipts.findAll {
+            it.date > payCheque.payPeriodStartDate && it.date <= payCheque.payPeriodEndDate
+        }
+        .collect { it.amount * commissionRate / 100 }.sum() ?: 0
+
+        return commissions + proRataSalary
     }
 }
--- payroll-case-study/src/test/groovy/thomashan/github/io/payroll/classification/CommissionedClassificationTests.groovy
+++ payroll-case-study/src/test/groovy/thomashan/github/io/payroll/classification/CommissionedClassificationTests.groovy
@@ -2,6 +2,7 @@ package thomashan.github.io.payroll.classification
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import thomashan.github.io.payroll.DateTimeUtil
 import thomashan.github.io.payroll.PayCheque
 import thomashan.github.io.payroll.SalesReceipt
 
@@ -11,10 +12,12 @@ class CommissionedClassificationTests {
     private LocalDate today = LocalDate.now()
     private CommissionedClassification commissionedClassification
     private SalesReceipt salesReceipt
+    private double salary = 1000
+    private double commissionRate = 20
 
     @BeforeEach
     void setUp() {
-        commissionedClassification = new CommissionedClassification(1000, 20)
+        commissionedClassification = new CommissionedClassification(salary, commissionRate)
         salesReceipt = new SalesReceipt(today, 30)
     }
 
@@ -24,11 +27,28 @@ class CommissionedClassificationTests {
     }
 
     @Test
-    void "calculatePay should return correct gross pay"() {
-        commissionedClassification.addSalesReceipt(salesReceipt)
+    void "calculatePay should return pro rata salary"() {
         PayCheque payCheque = new PayCheque(today.minusWeeks(2), today)
-        double grossPay = commissionedClassification.salary + salesReceipt.amount * commissionedClassification.commissionRate / 100
+        double grossPay = proRataSalary(salary)
 
         assert commissionedClassification.calculatePay(payCheque) == grossPay
+    }
+
+    @Test
+    void "calculatePay should return correct gross pay with all sales receipt within the pay cycle included"() {
+        commissionedClassification.addSalesReceipt(new SalesReceipt(today.minusWeeks(2), 70))
+        commissionedClassification.addSalesReceipt(new SalesReceipt(today.minusDays(1), 50))
+        commissionedClassification.addSalesReceipt(salesReceipt)
+        commissionedClassification.addSalesReceipt(new SalesReceipt(today.plusDays(1), 100))
+        PayCheque payCheque = new PayCheque(today.minusWeeks(2), today)
+        double proRataSalary = proRataSalary(salary)
+        double commissions = [50, salesReceipt.amount]*.multiply(commissionRate / 100).sum()
+        double grossPay = proRataSalary + commissions
+
+        assert commissionedClassification.calculatePay(payCheque) == grossPay
+    }
+
+    private double proRataSalary(double salary) {
+        return 14 / DateTimeUtil.daysInMonth(today) * salary
     }
 }
