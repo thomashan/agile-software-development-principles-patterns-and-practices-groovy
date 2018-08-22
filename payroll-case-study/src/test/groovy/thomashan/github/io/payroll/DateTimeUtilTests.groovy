8c1cebb2940576f241010f91f0f8867c9f9869fd
Thomas Han <thomas.han@live.com>
2018-08-22 15:29:49 +1000

    added test case for next day

--- payroll-case-study/src/test/groovy/thomashan/github/io/payroll/DateTimeUtilTests.groovy
+++ payroll-case-study/src/test/groovy/thomashan/github/io/payroll/DateTimeUtilTests.groovy
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class DateTimeUtilTests {
-    private LocalDate today = LocalDate.of(2018, 8, 17)
+    private LocalDate date = LocalDate.of(2018, 8, 17)
 
     @Test
     void "numberOfFridaysInPayPeriod should return 0 if start and end is the same"() {
-        assert DateTimeUtil.numberOfFridaysInPayPeriod(today, today) == 0
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date) == 0
+    }
+
+    @Test
+    void "numberOfFridaysInPayPeriod should return 0 if start day is Friday and end is next day"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date.plusDays(1)) == 0
     }
 
     @Test
     void "numberOfFridaysInPayPeriod should return correct"() {
-        assert DateTimeUtil.numberOfFridaysInPayPeriod(today, today.plusWeeks(1)) == 1
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date.plusWeeks(1)) == 1
     }
 
     @Test
