5790d9c5f7946ac2abc19e18f75227517cba5041
Thomas Han <thomas.han@live.com>
2018-11-24 23:13:44 +1100

    added test case for unknown payment classification

--- payroll-case-study/ui/zk/src/test/groovy/thomashan/github/io/payroll/ui/zk/list/EmployeeViewModelTests.groovy
+++ payroll-case-study/ui/zk/src/test/groovy/thomashan/github/io/payroll/ui/zk/list/EmployeeViewModelTests.groovy
@@ -2,6 +2,8 @@ package thomashan.github.io.payroll.ui.zk.list
 
 import org.junit.jupiter.api.Test
 import thomashan.github.io.payroll.Employee
+import thomashan.github.io.payroll.PayCheque
+import thomashan.github.io.payroll.classification.PaymentClassification
 import thomashan.github.io.payroll.transaction.add.AddCommissionedEmployee
 import thomashan.github.io.payroll.transaction.add.AddHourlyEmployee
 import thomashan.github.io.payroll.transaction.add.AddSalariedEmployee
@@ -33,5 +35,22 @@ class EmployeeViewModelTests implements EmployeeManagement {
         EmployeeViewModel employeeViewModel = new EmployeeViewModel(employee)
 
         assert employeeViewModel.waging == "1.0 / month + 15.0% sales"
+    }
+
+    @Test
+    void "unknown classification returns error"() {
+        new AddCommissionedEmployee(employeeId, "AnonName", "AnonAddress", 1, 15).execute()
+        Employee employee = payrollDatabase.getEmployee(employeeId)
+        employee.paymentClassification = new UnknownPaymentClassification()
+        EmployeeViewModel employeeViewModel = new EmployeeViewModel(employee)
+
+        assert employeeViewModel.waging == "Couldn't determine waging"
+    }
+
+    private static class UnknownPaymentClassification implements PaymentClassification {
+        @Override
+        double calculatePay(PayCheque payCheque) {
+            return 0
+        }
     }
 }
