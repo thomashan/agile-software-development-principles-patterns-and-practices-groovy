5cb0cd13759ed901f84a150123ad21a394869b01
Thomas Han <thomas.han@live.com>
2020-08-18 02:59:56 +1000

    extended tests

--- payroll-case-study/core/src/test/groovy/thomashan/github/io/payroll/DateTimeUtilTests.groovy
+++ payroll-case-study/core/src/test/groovy/thomashan/github/io/payroll/DateTimeUtilTests.groovy
@@ -5,21 +5,37 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class DateTimeUtilTests {
-    private LocalDate date = LocalDate.of(2018, 8, 17)
+    private LocalDate thursday = LocalDate.of(2018, 8, 16)
+    private LocalDate friday = LocalDate.of(2018, 8, 17)
 
     @Test
-    void "numberOfFridaysInPayPeriod should return 0 if start and end is the same"() {
-        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date) == 0
+    void "numberOfFridaysInPayPeriod should return 0 if start and end is the same thursday"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(thursday, thursday) == 0
+    }
+
+    @Test
+    void "numberOfFridaysInPayPeriod should return 0 if start and end is the same friday"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(friday, friday) == 0
     }
 
     @Test
     void "numberOfFridaysInPayPeriod should return 0 if start day is Friday and end is next day"() {
-        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date.plusDays(1)) == 0
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(friday, friday.plusDays(1)) == 0
     }
 
     @Test
-    void "numberOfFridaysInPayPeriod should return correct"() {
-        assert DateTimeUtil.numberOfFridaysInPayPeriod(date, date.plusWeeks(1)) == 1
+    void "numberOfFridaysInPayPeriod should return 0 if start day is thursday and end is friday"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(thursday, friday) == 1
+    }
+
+    @Test
+    void "numberOfFridaysInPayPeriod should return correct if stared on thursday and finished next thursday"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(thursday, thursday.plusWeeks(1)) == 1
+    }
+
+    @Test
+    void "numberOfFridaysInPayPeriod should return correct if started on friday and finished next friday"() {
+        assert DateTimeUtil.numberOfFridaysInPayPeriod(friday, friday.plusWeeks(1)) == 1
     }
 
     @Test
